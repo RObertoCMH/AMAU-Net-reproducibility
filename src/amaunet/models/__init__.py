7e947e817896a3f5e8734a63af8a9004c1dfec39
RObertoCMH <robertocarlos_mh14@hotmail.com>
2026-01-21 18:31:24 -0600

    Update __init__.py

--- src/amaunet/models/__init__.py
+++ src/amaunet/models/__init__.py
@@ -1,1 +1,11 @@
-#
+# src/amaunet/models/__init__.py
+from .blocks import Conv2dBlock
+from .attention import CBAM, SelfAttentionBlock
+from .unet import UNetWithAttention
+
+__all__ = [
+    "Conv2dBlock",
+    "CBAM",
+    "SelfAttentionBlock",
+    "UNetWithAttention",
+]
